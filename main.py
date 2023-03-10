47caa383ede8408fb107fd05c76ae89eaa3b35a2
Lambda Sangkala <lambdasangkala45@gmail.com>
2023-03-10 09:57:40 +0700

    update module show password

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import authentication
 import landing
 import menu
+import shieldpassword
 
 
 landing.init()
@@ -10,7 +11,6 @@ landing.init()
 print('Select Action:')
 for item in menu.data:
     print('[' + str(item['index']) + '] ' + item['name'])
-
 selectedAction = False
 while selectedAction == False:
     inputTargetFile = input("Select File [ID] : ")
@@ -18,5 +18,12 @@ while selectedAction == False:
     if (valueInputTargetFile < len(menu.data)):
         selectedAction = inputTargetFile
         print('Selected Action : ' + menu.data[valueInputTargetFile]['name'])
+        if valueInputTargetFile == 0:
+            shieldpassword.showPassword()
+        elif valueInputTargetFile == 1:
+            shieldpassword.addPassword()
+        elif valueInputTargetFile == 3:
+            shieldpassword.deletePassword()
+
     else:
         print('Invalid Index, try again...')
--- shieldpassword.py
+++ shieldpassword.py
@@ -1,34 +1,111 @@
 import json
 import os
 from cryptography.fernet import Fernet
+from prettytable import PrettyTable
+from datetime import datetime
+
+# arr = [
+#     {
+#         'platform': 'google',
+#         'username': 'lambdasangkala45@gmail.com',
+#         'password': 'test12345'
+#     }
+# ]
+
+# # key = Fernet.generate_key()
+
+# fernet = Fernet(key)
+# data_enc = json.dumps(arr)
+# ex = data_enc.encode()
+# encrypted = fernet.encrypt(ex)
+
+# # MODIFY FILE
+# f = open("secure.lambda", "w")
+# f.write(encrypted.decode())
+# print(encrypted)
+
+# # READ FILE
+# f = open("secure.lambda", "r")
+# todec = f.read().encode()
+# print(todec)
+
+# decrypted = fernet.decrypt(todec)
+# print(decrypted.decode())
 
 
 def showPassword():
-    arr = [
+    key = os.environ.get('FERNET_KEY')
+    fernet = Fernet(key.encode())
+
+    # Fetch Encrypt Data
+    f = open("secure.lambda", "r")
+    todec = f.read().encode()
+    decrypted = fernet.decrypt(todec).decode()
+    dataPassword = json.loads(decrypted)
+    # print(decrypted)
+    t = PrettyTable(['Platform', 'Group Type', 'Host', 'Username',
+                    'Password', 'Last Update'])
+    for item in dataPassword:
+        t.add_row([item['platform'], item['group'], item['host'], item['username'],
+                  item['password'], item['createdAt']])
+    print(t)
+
+
+def addPassword():
+    key = os.environ.get('FERNET_KEY')
+    fernet = Fernet(key.encode())
+
+    platform = input("Input Platform Name : ")
+    group = input("Input Group (Organization/Personal) : ")
+    host = input('Input Host : ')
+    username = input("Input Username : ")
+    password = input("Input Password : ")
+
+    now = datetime.now()
+    createdAt = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    data = [
         {
-            'platform': 'google',
-            'username': 'lambdasangkala45@gmail.com',
-            'password': 'test12345'
+            'platform': platform,
+            'group': group,
+            'host': host,
+            'username': username,
+            'password': password,
+            'createdAt': createdAt,
+            'trackingChanges': []
         }
     ]
 
-    # key = Fernet.generate_key()
-    key = os.environ.get('FERNET_KEY')
-
-    fernet = Fernet(key)
-    data_enc = json.dumps(arr)
+    data_enc = json.dumps(data)
     ex = data_enc.encode()
     encrypted = fernet.encrypt(ex)
-
-    # MODIFY FILE
+    # # MODIFY FILE
     f = open("secure.lambda", "w")
     f.write(encrypted.decode())
     print(encrypted)
 
-    # READ FILE
-    f = open("secure.lambda", "r")
-    todec = f.read().encode()
-    print(todec)
 
-    decrypted = fernet.decrypt(todec)
-    print(decrypted.decode())
+def deletePassword():
+    key = os.environ.get('FERNET_KEY')
+    fernet = Fernet(key.encode())
+
+    # # Fetch Encrypt Data
+    # f = open("secure.lambda", "r")
+    # todec = f.read().encode()
+    # decrypted = fernet.decrypt(todec).decode()
+    # dataPassword = json.loads(decrypted)
+    # # print(decrypted)
+    # t = PrettyTable(['Index','Platform', 'Username', 'Password'])
+    # idx = 0
+    # for item in dataPassword:
+    #     idx = idx + 1
+    #     t.add_row([idx,item['platform'],item['username'],item['password']])
+    # print(t)
+    # platform = input("Select Index to Remove : ")
+
+    # # MODIFY FILE
+    ex = []
+    encrypted = fernet.encrypt(json.dumps(ex).encode())
+    f = open("secure.lambda", "w")
+    f.write(encrypted.decode())
+    print(encrypted)
